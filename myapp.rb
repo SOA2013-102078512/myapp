15ce986842ea458ae16e877e5e0600af76f423bc
SOA2013-102078512 <elaine790916@hotmail.com>
2014-04-10 08:45:01 -0700

    demoapp4th

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,13 @@
-# myapp.rb
+require 'rubygems'
 require 'sinatra'
 
-get '/' do
-  'Hello world!'
-end
-get '/toSHA2/:input' do
+ 
+#get '/' do
+#   "Hello, World!"
+#    
+#  end
+
+  get '/toSHA2/:input' do
     inputstr = params[:input]
     hashresult= Digest::SHA256.hexdigest inputstr
     "SHA256: #{hashresult}"
